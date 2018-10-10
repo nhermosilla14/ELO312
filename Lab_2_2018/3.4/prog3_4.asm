89924c20bf8e7d481bc14997babd82213b7f75be
Nicolás Hermosilla <nhermosilla14@gmail.com>
2018-10-10 05:28:52 +0000

    Avanzada parte 3.4

---
+++ Lab_2_2018/3.4/prog3_4.asm
@@ -0,0 +1,75 @@
+#include "msp430x16x.h"
+#define         SPinit  0x24FA
+#define         COUNTER_LOOP    R8
+#define         COUNTER_ADDR   R9
+#define         n       0x0007
+ 
+main             
+;-------------------------------------------------------------------------------        
+                ORG	0x2500				; Dirección de inicio del programa
+;-------------------------------------------------------------------------------
+RESET           MOV	#SPinit,SP      	; Inicio del Stack
+;; Se inicializa una porción de memoria, para probar la copia
+                MOV     #n,R14            ; Inicialización del contador
+                
+                PUSH    R10
+LOOP1           MOV     #0x81,R10
+                ADD     R14,R10
+                PUSH    R10                   ; Añade el valor 0x81+R14 al stack
+                DEC     R14               ; Disminuye el contador en 1
+                
+                JNZ     LOOP1                    ;
+
+; Aquí empieza la función nueva
+
+                POP     R10
+                MOV     #0x2620,R12 
+                MOV     SP,R13
+                MOV     #n,R14
+                
+                CALL    #FUNCION
+                
+;; Parte para limpiar la memoria usada al inicio
+                MOV     #n,R14
+LOOP3           POP     R10                   ; Añade el valor 0x81+R14 al stack
+                DEC     R14               ; Disminuye el contador en 1  
+                JNZ     LOOP3                    ;
+
+                BR      #EXIT
+
+FUNCION         BIS.B   #01h,&P1DIR
+                BIS.B   #00h,&P1SEL
+                PUSH    R10                     ; Respalda R10 en el stack
+                PUSH    COUNTER_ADDR            ; Respalda COUNTER_ADDR en el stack
+                PUSH    COUNTER_LOOP            ; Respalda COUNTER_LOOP en el stack
+                MOV     #0x0,COUNTER_ADDR       ; Inicializa en 0 COUNTER_ADDR
+                MOV     R14,COUNTER_LOOP        ; Inicializa en el valor dado el
+                DEC     COUNTER_LOOP                                 ; COUNTER_LOOP
+LOOP2           BIC.B   #10h,&P1OUT
+                MOV     R13,R15                 ; COPIA R13 a R15, para operar en R15
+                ADD     COUNTER_ADDR,R15        ; SUMA COUNTER_ADDR a R15
+                MOV     @R15,R10                ; Rescata el valor de R15 en R10
+                MOV     R12,R15                 ; Reusamos R15 para seguir operando
+                ADD     COUNTER_ADDR, R15  
+                BIS.B   #10h,&P1OUT     
+                MOV     R10,0x0(R15)            ; Copia el valor guardado a la
+                INC     COUNTER_ADDR            ; nueva dirección.
+                INC     COUNTER_ADDR            ;
+                DEC     COUNTER_LOOP            ; Sube el contador de addr, baja
+                JNZ     LOOP2                   ; el del LOOP.
+                MOV     R12,R12                 ; Valor de retorno a R12
+                POP     COUNTER_LOOP            ; Restituye los valores respaldados
+                POP     COUNTER_ADDR            ; en el stack.
+                POP     R10                     
+                RET
+                
+                
+;-------------------------------------------------------------------------------
+EXIT            JMP     EXIT            ;
+;------------------------------------------------------------------------------ 
+; Vector de interrupción asociado a Reset 
+;------------------------------------------------------------------------------ 
+                ORG 0xFFFE ; MSP430 RESET Vector 
+                DW RESET
+                END
+ 
